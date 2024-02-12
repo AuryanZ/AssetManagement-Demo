70e6fba9a1b3966d25997a8c2516e96ff362e6c5
Rui Zeng <rzeng0918@gmail.com>
2024-02-13 11:15:11 +1300

    useEffect issue

--- asset-management-client/src/api/assetsAPI.ts
+++ asset-management-client/src/api/assetsAPI.ts
@@ -1,11 +1,13 @@
 import api from "../../services/api";
 
 export const getAssets = async () => {
+    console.log("+++++++++")
     try {
-        const response = await api.get('/api/assets');
+        const response = await api.get('/assets');
+        console.log(response.data);
         return response.data;
     }
     catch (error) {
-        console.error(error);
+        console.error("Failed to fetch data",error);
     }
 }
--- asset-management-client/src/app/page.tsx
+++ asset-management-client/src/app/page.tsx
@@ -1,35 +1,42 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Header from "../../components/Header/header";
+import { getAssets } from "@/api/assetsAPI";
 
 export default function Home() {
 
   const [assetsData, setAssetsData] = useState(null);
+  console.log("console log from Home.tsx");
 
   useEffect(() => {
+    console.log("useEffect");
     const fetchAssets = async () => {
+      console.log("fetching data...")
       try {
-        const response = await fetch('/api/assets');
-        const data = await response.json();
+        console.log("Trying to fetch data...");
+        const data = await getAssets();
         setAssetsData(data);
-      } catch {
+      } catch (error) {
         console.error('Error fetching data');
       }
     };
     fetchAssets();
   }, []);
+  console.log("start rander DOM")
 
-      return (
-        <div className="App">
-          <Header />
-          <div className='container mx-auto'>
-            {assetsData && (assetsData as any[]).map((asset) => (
-              <div key={asset.id}>
-                <h2>{asset.AssetName}</h2>
-                <p>{asset.AssetStatus}</p>
-              </div>
-            ))}
+  // console.log(assetsData);
+
+  return (
+    <div className="App">
+      <Header />
+      <div className='container mx-auto'>
+        {assetsData && (assetsData as any[]).map((asset) => (
+          <div key={asset.id}>
+            <h2>{asset.AssetName}</h2>
+            <p>{asset.AssetStatus}</p>
           </div>
-        </div>
-      );
-    }
+        ))}
+      </div>
+    </div>
+  );
+}
