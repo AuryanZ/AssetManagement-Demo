62f9b0f86d688087b19b91c37aba16624d8210cb
Rui Zeng <rzeng0918@gmail.com>
2024-02-13 10:20:25 +1300

    fetch data

--- asset-management-client/src/app/page.tsx
+++ asset-management-client/src/app/page.tsx
@@ -1,13 +1,35 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/Header/header";
 
 export default function Home() {
-  
-  return (
-    <div className="App">
-      <Header />
-      <div className='container mx-auto'>
-        Hello World
-      </div>
-    </div>
-  );
-}
+
+  const [assetsData, setAssetsData] = useState(null);
+
+  useEffect(() => {
+    const fetchAssets = async () => {
+      try {
+        const response = await fetch('/api/assets');
+        const data = await response.json();
+        setAssetsData(data);
+      } catch {
+        console.error('Error fetching data');
+      }
+    };
+    fetchAssets();
+  }, []);
+
+      return (
+        <div className="App">
+          <Header />
+          <div className='container mx-auto'>
+            {assetsData && (assetsData as any[]).map((asset) => (
+              <div key={asset.id}>
+                <h2>{asset.AssetName}</h2>
+                <p>{asset.AssetStatus}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    }
